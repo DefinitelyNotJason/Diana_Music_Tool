413f744a8a984b2b96520377a54388ab4b14abfa
DefinitelyNotJason <jtan257@uwo.ca>
2022-12-02 22:45:56 -0500

    Delete console.log backend

--- routes/user_route.js
+++ routes/user_route.js
@@ -224,7 +224,6 @@ function generateAccessToken(user) {
 //check user login
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
-    console.log(authHeader);
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null){
         console.log('No token!');
